e930e4f6833506fec4a7ca9f16b55968561d70e8
Nicolas Parrilla <nicolasparrillageniz@gmail.com>
2024-05-11 16:23:38 +0200

    no funciona post todavia

--- VideoMinerTemplate-main/VideoMinerTemplate-main/src/main/java/aiss/videominer/controller/ChannelController.java
+++ VideoMinerTemplate-main/VideoMinerTemplate-main/src/main/java/aiss/videominer/controller/ChannelController.java
@@ -1,14 +1,14 @@
 package aiss.videominer.controller;
 
-import aiss.videominer.model.Channel;
-import aiss.videominer.model.Video;
-import aiss.videominer.repository.ChannelRepository;
+import aiss.videominer.model.*;
+import aiss.videominer.repository.*;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,6 +17,16 @@ import java.util.Optional;
 public class ChannelController {
     @Autowired
     ChannelRepository repository;
+
+    @Autowired
+    VideoRepository videoRepository;
+
+    @Autowired
+    CommentRepository commentRepository;
+    @Autowired
+    CaptionRepository captionRepository;
+    @Autowired
+    UserRepository userRepository;
 
     @Autowired
     VideoController videoController;
@@ -35,14 +45,43 @@ public class ChannelController {
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public Channel create(@Valid @RequestBody Channel channel) {
-        Channel canal = repository.save(new Channel(channel.getName(), channel.getDescription(), channel.getCreatedTime()));
-        parseVideos(canal);
-        return canal;
+        List<Video> videos = parseVideos(channel);
+        Channel canal = new Channel(channel.getName(), channel.getDescription(), channel.getCreatedTime());
+        canal.setVideos(videos);
+        return repository.save(canal);
     }
 
-    private void parseVideos(Channel channel){
+    private List<Video> parseVideos(Channel channel){
+        List<Video> res = new ArrayList<>();
         for (Video v : channel.getVideos()){
-            videoController.create(channel.getId(), v);
+            List<Comment> comments = parseComment(v);
+            parseCaption(v);
+            Video video = new Video(v.getName(), v.getDescription(), v.getReleaseTime());
+            video.setCaptions(v.getCaptions());
+            video.setComments(comments);
+            videoRepository.save(video);
+            res.add(video);
+        }
+        return res;
+    }
+
+    private List<Comment> parseComment(Video v){
+        List<Comment> res = new ArrayList<>();
+        for (Comment c : v.getComments()){
+            User user = c.getAuthor();
+            userRepository.save(user);
+
+            Comment comment = new Comment(c.getText(), c.getCreatedOn());
+            comment.setAuthor(user);
+            commentRepository.save(comment);
+            res.add(comment);
+        }
+        return res;
+    }
+
+    private void parseCaption(Video v){
+        for (Caption c : v.getCaptions()){
+            captionRepository.save(c);
         }
     }
 
